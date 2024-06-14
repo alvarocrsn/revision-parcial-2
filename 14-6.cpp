5faa3a658518d62d0bc8d7f2860e22094cae9074
“atsanchez” <atsanchez@frsn.utn.edu.ar>
2024-06-14 15:30:52 -0300

    first commit

---
+++ 14-6.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/// el void puede devolver algo o nada
+void imprimirArray(vector<int>unArray){
+    for(int i = 0; i < unArray.size(); i++){
+        cout << unArray[i] << " ";
+    }
+    cout << endl;
+}
+
+int main(){
+    ///parte 1
+    vector<int> numeros = {10, 20, 30, 40, 50};
+    cout << "Vector original: ";
+    imprimirArray(numeros);
+
+    ///parte 2, agregar valores
+    numeros.push_back(60);
+    numeros.push_back(70);
+    cout << "El vector despues de pushear los datos: ";
+    imprimirArray(numeros);
+
+    ///parte 3 quitar valores
+    vector<int>nuevoArray;
+    size_t index = 0;
+    for(const int elementos : numeros){
+        if(index != 2){
+            nuevoArray.push_back(elementos);
+        }
+        index++;
+    }
+    cout << "Array sin el 2do elemento: ";
+    imprimirArray(nuevoArray);
+
+    return 0;
+}
